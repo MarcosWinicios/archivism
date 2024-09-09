4c51dd473b28534c5fa3c993a822a712f600e4da
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-09-09 02:57:40 -0300

    feat - Changing class to generate list files generically

--- src/main/java/com/marcos/archivism/service/GenerateFilesService.java
+++ src/main/java/com/marcos/archivism/service/GenerateFilesService.java
@@ -10,43 +10,41 @@ import java.util.List;
 public class GenerateFilesService {
     private final static String OUTPUT_DIRECTORY = "outputFiles/";
 
-    public void generateFile(List<String> startList, List<String> simpleNumeric, List<String> terminalDigitNumeric, List<String> chronologic) {
-        this.generateFile(startList, simpleNumeric, terminalDigitNumeric, chronologic, "organizedFiles.csv");
+    public void generateFile(List<String> originalList, List<String> targetList) {
+        this.generateFile(originalList, targetList, "organizedFiles.csv");
     }
 
-        public void generateFile(List<String> startList, List<String> simpleNumeric, List<String> terminalDigitNumeric, List<String> chronologic, String fileName) {
-        int listSize = getListSize(startList, simpleNumeric, terminalDigitNumeric, chronologic);
-        List<String[]> data = generateData(startList, simpleNumeric, terminalDigitNumeric, chronologic, listSize);
+        public void generateFile(List<String> originalList, List<String> targetList, String fileName) {
+        int listSize = getListSize(originalList, targetList);
+
+        List<String[]> data = generateData(originalList, targetList, listSize);
 
         CsvUtil.printCsvLines(data);
 
         CsvUtil.generateCsvFile(data, OUTPUT_DIRECTORY, fileName);
 
-
     }
 
-    private List<String[]> generateData(List<String> startList, List<String> simpleNumeric, List<String> numericTerminalDigit, List<String> chronologic, int lines){
+    private List<String[]> generateData(List<String> originalList, List<String> targetList, int lines){
         List<String[]> result =  new ArrayList<>();
-        String[] header = new String[]{"Lista Inicial", "Simples", "Dígito terminal", "Cronológico"};
+        String[] header = new String[]{"Lista original", "Lista ordenada"};
 
         for(int i = 0; i < lines; i++){
             result.add(new String[]{
-                    startList.get(i),
-                    ArchivismUtil.addSeparator(simpleNumeric.get(i), " "),
-                    numericTerminalDigit.get(i),
-                    chronologic.get(i)
+                    originalList.get(i),
+                    ArchivismUtil.addSeparator(targetList.get(i), " ")
             });
         }
         result.addFirst(header);
         return result;
     }
 
-    private int getListSize(List<String> startList, List<String> simpleNumeric, List<String> numericTerminalDigit, List<String> chronologic){
-        if(startList.size() == simpleNumeric.size() && startList.size() == numericTerminalDigit.size() && startList.size() == chronologic.size()){
-            return startList.size();
+    private int getListSize(List<String> originalList, List<String> targetList){
+        if(originalList.size() == targetList.size()){
+            return originalList.size();
         }
 
-        throw new RuntimeException("As listas geradas possuem tamanho diferente da orginal");
+        throw new RuntimeException("As lista gerada possue tamanho diferente da original");
     }
 
 }
