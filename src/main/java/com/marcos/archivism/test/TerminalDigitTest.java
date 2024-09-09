aeca21deb362a34601dbcba274dede6f71c1b647
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-09-09 03:00:20 -0300

    feat - Changing test class to use only the terminal digit method

---
+++ src/main/java/com/marcos/archivism/test/TerminalDigitTest.java
@@ -0,0 +1,29 @@
+package com.marcos.archivism.test;
+
+import com.marcos.archivism.service.GenerateFilesService;
+import com.marcos.archivism.utils.ArchivismUtil;
+import com.marcos.archivism.utils.CsvUtil;
+import com.marcos.archivism.utils.ListUtils;
+
+import java.util.List;
+
+public class TerminalDigitTest {
+    public static void main(String[] args) {
+        List<String[]> readData = CsvUtil.readCsvFile("file1.csv");
+
+        System.out.println("Tamanho da lista inicial: " + readData.size());
+        List<String> originalList = ListUtils.handleListArrayToList(readData);
+        List<String> terminalNumericList = ArchivismUtil.terminalDigitNumericMethod(originalList);
+
+//        ArchivismUtil.printTerminalNumericList(terminalNumericList);
+
+        GenerateFilesService service = new GenerateFilesService();
+        service.generateFile(originalList, terminalNumericList);
+    }
+
+
+
+
+
+
+}
